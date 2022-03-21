8d2bbeb2eca04424fa63b0cd92f93e6f729c1ce8
paogutierrez <paola.gutierrez.yaquibe@gmail.com>
2022-03-21 09:19:18 -0500

    estilos pagina Inicio

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -36,14 +36,30 @@ const MenuNavegacion = () => {
         </Container>
       </Navbar>
       <Imagen />
-      <div>
+      <div className="contenedorParrafo">
+        <h1>Tips</h1>
         <p className="parrafoInicial">
-          ¿Sin ideas para elaborar recetas saludables? No te preocupes, porque
-          desde PUMPKIN te dejamos recetas de platos saludables y fáciles de
-          hacer perfectos para enfrentar las altas temperaturas sin renunciar a
-          la dieta sana y equilibrada. Prepara papel y bolígrafo y apunta todos
-          los ingredientes que necesitas para preparar tu menú saludable y muy
-          rico.
+          Para ti que te encanta cocinar, Recetas PUMPKIN te trae diferentes
+          recetas para todos los momentos y para que mejores tus habilidades y
+          sorprendas a todos con estas deliciosas preparaciones. Dependiendo de
+          lo que vayas a preparar necesitarás diferentes implementos. Estos son
+          algunos consejos para estar siempre bien preparado:
+          <br></br> 
+          <br></br> 
+          Lee la
+          receta de forma completa.<br></br> Antes de cocinar despeja el
+          ambiente.<br></br> Alista los ingredientes para que estén a la mano.{" "}
+          <br></br>Limpia la cocina mientras vas cocinando.<br></br> Evita
+          distracciones mientras cocines.
+          <br></br> 
+          <br></br> 
+          Si empiezas a tener en cuenta
+          estas recomendaciones vas a notar lo bien que te va en la cocina, cómo
+          mejoras tus técnicas y de esta forma las cosas saldrán bien obteniendo
+          resultados deliciosos. Descubre la categoría de platos fuertes con
+          todas las recetas deliciosas que puedes preparar en tu casa con
+          ingredientes que son muy fáciles de conseguir. Si necesitas ideas para
+          preparar platos fuertes, esta categoría es perfecta para ti.
         </p>
       </div>
     </div>
